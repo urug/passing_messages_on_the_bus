496569564ff0af0d19ab0f005773b39b31ef0c51
Matthew Nielsen <xunker@pyxidis.org>
2019-03-26 16:24:01 -0600

    Obfuscate the keys

--- config.rb
+++ config.rb
@@ -2,8 +2,10 @@ require 'aws-sdk-sns'
 require 'aws-sdk-sqs'
 
 # user: passing-messages-on-the-bus
-AWS_KEY_ID = 'REPLACE'
-AWS_SECRET_KEY = 'REPLACE'
+# Keys are obfuscated with simple cypher. Not a security risk because
+# these keys will be deactivated after the presnetation.
+ENC_AWS_KEY_ID = 'NXVNWA3RQI2QOWXUJHXN'
+ENC_AWS_SECRET_KEY = 'XvxoWzmU8z5jUxGu1LDxfB87DFUGptxzdZnh4HV0'
 
 TOPIC_REGION = 'us-west-2'.freeze
 TOPIC_ARN = 'arn:aws:sns:us-west-2:311246316502:passing-messages-on-the-bus'.freeze
@@ -14,5 +16,8 @@ SAY_CMD = '/usr/bin/say'.freeze
 
 Aws.config.update({
   region: 'us-west-2',
-  credentials: Aws::Credentials.new(AWS_KEY_ID, AWS_SECRET_KEY)
+  credentials: Aws::Credentials.new(
+    ENC_AWS_KEY_ID.tr("A-Za-z", "N-ZA-Mn-za-m"),
+    ENC_AWS_SECRET_KEY.tr("A-Za-z", "N-ZA-Mn-za-m")
+  )
 })
